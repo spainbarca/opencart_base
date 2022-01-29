5956b1ba30c6d502ccb61731ce80dce05d775b11
spainbarca <spain.barcelona.1999@gmail.com>
2022-01-29 01:47:37 -0500

    Agregar Idioma Español

---
+++ storage/cache/template/34/34480d95b758e550010b80a05bab2a8e1f06bfa4a1f3c147063c2919ff55116a.php
@@ -0,0 +1,294 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* catalog/download_form.twig */
+class __TwigTemplate_8007dcbb95c550fbbf4ae5832f46803bfd638f1a42f694d02fa91fcb5113b7eb extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo ($context["header"] ?? null);
+        echo ($context["column_left"] ?? null);
+        echo "
+<div id=\"content\">
+  <div class=\"page-header\">
+    <div class=\"container-fluid\">
+      <div class=\"pull-right\">
+        <button type=\"submit\" form=\"form-download\" data-toggle=\"tooltip\" title=\"";
+        // line 6
+        echo ($context["button_save"] ?? null);
+        echo "\" class=\"btn btn-primary\"><i class=\"fa fa-save\"></i></button>
+        <a href=\"";
+        // line 7
+        echo ($context["cancel"] ?? null);
+        echo "\" data-toggle=\"tooltip\" title=\"";
+        echo ($context["button_cancel"] ?? null);
+        echo "\" class=\"btn btn-default\"><i class=\"fa fa-reply\"></i></a></div>
+      <h1>";
+        // line 8
+        echo ($context["heading_title"] ?? null);
+        echo "</h1>
+      <ul class=\"breadcrumb\">
+        ";
+        // line 10
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["breadcrumbs"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["breadcrumb"]) {
+            // line 11
+            echo "        <li><a href=\"";
+            echo twig_get_attribute($this->env, $this->source, $context["breadcrumb"], "href", [], "any", false, false, false, 11);
+            echo "\">";
+            echo twig_get_attribute($this->env, $this->source, $context["breadcrumb"], "text", [], "any", false, false, false, 11);
+            echo "</a></li>
+        ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['breadcrumb'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 13
+        echo "      </ul>
+    </div>
+  </div>
+  <div class=\"container-fluid\">
+    ";
+        // line 17
+        if (($context["error_warning"] ?? null)) {
+            // line 18
+            echo "    <div class=\"alert alert-danger alert-dismissible\"><i class=\"fa fa-exclamation-circle\"></i> ";
+            echo ($context["error_warning"] ?? null);
+            echo "
+      <button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>
+    </div>
+    ";
+        }
+        // line 22
+        echo "    <div class=\"panel panel-default\">
+      <div class=\"panel-heading\">
+        <h3 class=\"panel-title\"><i class=\"fa fa-pencil\"></i> ";
+        // line 24
+        echo ($context["text_form"] ?? null);
+        echo "</h3>
+      </div>
+      <div class=\"panel-body\">
+        <form action=\"";
+        // line 27
+        echo ($context["action"] ?? null);
+        echo "\" method=\"post\" enctype=\"multipart/form-data\" id=\"form-download\" class=\"form-horizontal\">
+          <div class=\"form-group required\">
+            <label class=\"col-sm-2 control-label\">";
+        // line 29
+        echo ($context["entry_name"] ?? null);
+        echo "</label>
+            <div class=\"col-sm-10\">
+              ";
+        // line 31
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["languages"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["language"]) {
+            // line 32
+            echo "              <div class=\"input-group\"> <span class=\"input-group-addon\"><img src=\"language/";
+            echo twig_get_attribute($this->env, $this->source, $context["language"], "code", [], "any", false, false, false, 32);
+            echo "/";
+            echo twig_get_attribute($this->env, $this->source, $context["language"], "code", [], "any", false, false, false, 32);
+            echo ".png\" title=\"";
+            echo twig_get_attribute($this->env, $this->source, $context["language"], "name", [], "any", false, false, false, 32);
+            echo "\" /></span>
+                <input type=\"text\" name=\"download_description[";
+            // line 33
+            echo twig_get_attribute($this->env, $this->source, $context["language"], "language_id", [], "any", false, false, false, 33);
+            echo "][name]\" value=\"";
+            echo (((($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4 = ($context["download_description"] ?? null)) && is_array($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4) || $__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4 instanceof ArrayAccess ? ($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4[twig_get_attribute($this->env, $this->source, $context["language"], "language_id", [], "any", false, false, false, 33)] ?? null) : null)) ? (twig_get_attribute($this->env, $this->source, (($__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144 = ($context["download_description"] ?? null)) && is_array($__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144) || $__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144 instanceof ArrayAccess ? ($__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144[twig_get_attribute($this->env, $this->source, $context["language"], "language_id", [], "any", false, false, false, 33)] ?? null) : null), "name", [], "any", false, false, false, 33)) : (""));
+            echo "\" placeholder=\"";
+            echo ($context["entry_name"] ?? null);
+            echo "\" class=\"form-control\" />
+              </div>
+              ";
+            // line 35
+            if ((($__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b = ($context["error_name"] ?? null)) && is_array($__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b) || $__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b instanceof ArrayAccess ? ($__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b[twig_get_attribute($this->env, $this->source, $context["language"], "language_id", [], "any", false, false, false, 35)] ?? null) : null)) {
+                // line 36
+                echo "              <div class=\"text-danger\">";
+                echo (($__internal_68aa442c1d43d3410ea8f958ba9090f3eaa9a76f8de8fc9be4d6c7389ba28002 = ($context["error_name"] ?? null)) && is_array($__internal_68aa442c1d43d3410ea8f958ba9090f3eaa9a76f8de8fc9be4d6c7389ba28002) || $__internal_68aa442c1d43d3410ea8f958ba9090f3eaa9a76f8de8fc9be4d6c7389ba28002 instanceof ArrayAccess ? ($__internal_68aa442c1d43d3410ea8f958ba9090f3eaa9a76f8de8fc9be4d6c7389ba28002[twig_get_attribute($this->env, $this->source, $context["language"], "language_id", [], "any", false, false, false, 36)] ?? null) : null);
+                echo "</div>
+              ";
+            }
+            // line 38
+            echo "              ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['language'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 39
+        echo "            </div>
+          </div>
+          <div class=\"form-group required\">
+            <label class=\"col-sm-2 control-label\" for=\"input-filename\"><span data-toggle=\"tooltip\" title=\"";
+        // line 42
+        echo ($context["help_filename"] ?? null);
+        echo "\">";
+        echo ($context["entry_filename"] ?? null);
+        echo "</span></label>
+            <div class=\"col-sm-10\">
+              <div class=\"input-group\">
+                <input type=\"text\" name=\"filename\" value=\"";
+        // line 45
+        echo ($context["filename"] ?? null);
+        echo "\" placeholder=\"";
+        echo ($context["entry_filename"] ?? null);
+        echo "\" id=\"input-filename\" class=\"form-control\" />
+                <span class=\"input-group-btn\">
+                <button type=\"button\" id=\"button-upload\" data-loading-text=\"";
+        // line 47
+        echo ($context["text_loading"] ?? null);
+        echo "\" class=\"btn btn-primary\"><i class=\"fa fa-upload\"></i> ";
+        echo ($context["button_upload"] ?? null);
+        echo "</button>
+                </span> </div>
+              ";
+        // line 49
+        if (($context["error_filename"] ?? null)) {
+            // line 50
+            echo "              <div class=\"text-danger\">";
+            echo ($context["error_filename"] ?? null);
+            echo "</div>
+              ";
+        }
+        // line 52
+        echo "            </div>
+          </div>
+          <div class=\"form-group required\">
+            <label class=\"col-sm-2 control-label\" for=\"input-mask\"><span data-toggle=\"tooltip\" title=\"";
+        // line 55
+        echo ($context["help_mask"] ?? null);
+        echo "\">";
+        echo ($context["entry_mask"] ?? null);
+        echo "</span></label>
+            <div class=\"col-sm-10\">
+              <input type=\"text\" name=\"mask\" value=\"";
+        // line 57
+        echo ($context["mask"] ?? null);
+        echo "\" placeholder=\"";
+        echo ($context["entry_mask"] ?? null);
+        echo "\" id=\"input-mask\" class=\"form-control\" />
+              ";
+        // line 58
+        if (($context["error_mask"] ?? null)) {
+            // line 59
+            echo "              <div class=\"text-danger\">";
+            echo ($context["error_mask"] ?? null);
+            echo "</div>
+              ";
+        }
+        // line 61
+        echo "            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+<script type=\"text/javascript\"><!--
+\$('#button-upload').on('click', function() {
+\t\$('#form-upload').remove();
+
+\t\$('body').prepend('<form enctype=\"multipart/form-data\" id=\"form-upload\" style=\"display: none;\"><input type=\"file\" name=\"file\" /></form>');
+
+\t\$('#form-upload input[name=\\'file\\']').trigger('click');
+
+\tif (typeof timer != 'undefined') {
+    \tclearInterval(timer);
+\t}
+
+\ttimer = setInterval(function() {
+\t\tif (\$('#form-upload input[name=\\'file\\']').val() != '') {
+\t\t\tclearInterval(timer);
+
+\t\t\t\$.ajax({
+\t\t\t\turl: 'index.php?route=catalog/download/upload&user_token=";
+        // line 84
+        echo ($context["user_token"] ?? null);
+        echo "',
+\t\t\t\ttype: 'post',
+\t\t\t\tdataType: 'json',
+\t\t\t\tdata: new FormData(\$('#form-upload')[0]),
+\t\t\t\tcache: false,
+\t\t\t\tcontentType: false,
+\t\t\t\tprocessData: false,
+\t\t\t\tbeforeSend: function() {
+\t\t\t\t\t\$('#button-upload').button('loading');
+\t\t\t\t},
+\t\t\t\tcomplete: function() {
+\t\t\t\t\t\$('#button-upload').button('reset');
+\t\t\t\t},
+\t\t\t\tsuccess: function(json) {
+\t\t\t\t\tif (json['error']) {
+\t\t\t\t\t\talert(json['error']);
+\t\t\t\t\t}
+
+\t\t\t\t\tif (json['success']) {
+\t\t\t\t\t\talert(json['success']);
+
+\t\t\t\t\t\t\$('input[name=\\'filename\\']').val(json['filename']);
+\t\t\t\t\t\t\$('input[name=\\'mask\\']').val(json['mask']);
+\t\t\t\t\t}
+\t\t\t\t},
+\t\t\t\terror: function(xhr, ajaxOptions, thrownError) {
+\t\t\t\t\talert(thrownError + \"\\r\\n\" + xhr.statusText + \"\\r\\n\" + xhr.responseText);
+\t\t\t\t}
+\t\t\t});
+\t\t}
+\t}, 500);
+});
+//--></script></div>
+";
+        // line 117
+        echo ($context["footer"] ?? null);
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "catalog/download_form.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  270 => 117,  234 => 84,  209 => 61,  203 => 59,  201 => 58,  195 => 57,  188 => 55,  183 => 52,  177 => 50,  175 => 49,  168 => 47,  161 => 45,  153 => 42,  148 => 39,  142 => 38,  136 => 36,  134 => 35,  125 => 33,  116 => 32,  112 => 31,  107 => 29,  102 => 27,  96 => 24,  92 => 22,  84 => 18,  82 => 17,  76 => 13,  65 => 11,  61 => 10,  56 => 8,  50 => 7,  46 => 6,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "catalog/download_form.twig", "");
+    }
+}
